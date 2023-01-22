a3e32a4495e7da3d9451b50425ad8d13b6da6c99
Randolf Santiago <randolf_santiago85@yahoo.com>
2023-01-22 18:39:26 +0800

    Working on views and models

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from .models import Profile
 
 def index(request):
     return render(request, 'index.html', {})
@@ -22,6 +23,11 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
+
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+
                 return redirect('index')
         else:
             messages.info(request, 'Password not match')
